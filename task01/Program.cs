e871e9a7122d588953cc827c61a84888e73d60eb
alekandelav <140648372+AleksandrLAV@users.noreply.github.com>
2023-08-03 16:04:05 +0300

    add task01

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,12 +1,12 @@
 ﻿Console.WriteLine("введите число первое");
-int number1 = Convert.ToInt32(Console.RedLine());
+int number1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("введите число второе");
-int number2 = Convert.ToInt32(Console.RedLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
 
 if (number1/number2 == number2)
 {
- Console.WriteLine("да");
+    Console.WriteLine("да");
 }
 
 else
