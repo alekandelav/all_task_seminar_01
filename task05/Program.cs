70cf8228ff654ccfb7dac91c3d0c9c64dc6f3fa2
alekandelav <140648372+AleksandrLAV@users.noreply.github.com>
2023-08-04 12:35:19 +0300

    Напишите программу, которая принимает на вход Трехзначное число и на выходе показывает последнюю цифру этого числа

--- task05/Program.cs
+++ task05/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! ");
+﻿// Напишите программу, которая принимает на вход
+// Трехзначное число и на выходе показывает последнюю цифру
+// этого числа
+//456 -> 6
+//782 -> 2
+//918 -> 8
+
+//Вход
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int lastDigit = number%10;
+//выход
+Console.WriteLine($"Последняя цифра {lastDigit}");
