c2911adef4dab1ea4ca898af22c9e927725cfe91
alekandelav <140648372+AleksandrLAV@users.noreply.github.com>
2023-08-04 14:33:25 +0300

    save task01

--- lessons#2/task01/Program.cs
+++ lessons#2/task01/Program.cs
@@ -5,3 +5,17 @@
 //782 -> 72
 //918 -> 98
 
+int number = new Random().Next(10, 1000); // 99 + 1
+Console.WriteLine($"Случайное число из диапазона 10-999 -> {number}");
+
+int TwoNumbers = TwoNumbers(number);
+Console.WriteLine(TwoNumbers);
+
+
+int TwoNumbers (int num)
+{
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+int result = firstDigit * 10 + lastDigit;
+return result;
+}
