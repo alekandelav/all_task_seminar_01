267d16074cab68b1d528c19930c73bcbafac6592
alekandelav <140648372+AleksandrLAV@users.noreply.github.com>
2023-08-04 11:21:47 +0300

    commit + 4задание На вход принимает число и выдает его в квадрат число умноженное на само себя

---
+++ task04/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+//Напишите программу которая на вход принимает
+//Одно число (N), а на выходе показывает все целые
+//числа в промежутке от -N до N.
+//4 -> "-4, -3, -2, -1, 0, 1 , 2, 3, 4"
+//2 -> "-2, -1, 0, 1, 2"
+
+Console.Writeline("Введите целое число");
+int number = Conver.ToInt32(Console.ReadLine());
+int count = -number;
+while (Coynt <= number)
+{
+    console.Write($"{count}");
+    count++;
+}
