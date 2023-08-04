f07d0cecd0c5e4c0200071bbfaba158c6a5acf77
alekandelav <140648372+AleksandrLAV@users.noreply.github.com>
2023-08-04 12:11:49 +0300

    Напишите программу которая на вход принимает Одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.

--- task04/Program.cs
+++ task04/Program.cs
@@ -1,17 +1,15 @@
-﻿
+﻿// Напишите программу которая на вход принимает
+// Одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1 , 2, 3, 4"
+// 2 -> "-2, -1, 0, 1, 2"
 
-
-//Напишите программу которая на вход принимает
-//Одно число (N), а на выходе показывает все целые
-//числа в промежутке от -N до N.
-//4 -> "-4, -3, -2, -1, 0, 1 , 2, 3, 4"
-//2 -> "-2, -1, 0, 1, 2"
-
-Console.Writeline("Введите целое число");
-int number = Conver.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
 int count = -number;
-while (Coynt <= number)
+
+while (count <= number)
 {
-    console.Write($"{count}");
+    Console.Write($"{count}");
     count++;
 }
