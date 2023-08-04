523d9bf99872d8863bf3e785f0f819fc370efacc
alekandelav <140648372+AleksandrLAV@users.noreply.github.com>
2023-08-04 10:59:44 +0300

    Сделали 3 задание дни недели

--- task02/Program.cs
+++ task02/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет выдвать
+// Дни недели по заданному номеру
+
+// 3-> Среда
+// 5-> Пятница
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите номер от 01 до 07 номер недели");
+        string num = Console.ReadLine();
+
+        if (num == "1")Console.WriteLine("Понедельник");
+        if (num == "2") Console.WriteLine("Вторник");
+        if (num == "3") Console.WriteLine("Среда");
+        if (num == "4") Console.WriteLine("Четверг");
+        if (num == "5") Console.WriteLine("Пятница");
+        if (num == "6") Console.WriteLine("Суббота");
+        if (num == "7") Console.WriteLine("Воскресенье");
+
+        else Console.WriteLine("Error - 440");
+    }
+}
