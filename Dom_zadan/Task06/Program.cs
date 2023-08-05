84806385708aef2f2de1fbceb1c28be2b6c36c66
alekandelav <140648372+AleksandrLAV@users.noreply.github.com>
2023-08-05 10:56:04 +0300

    является ли число чётным

--- Dom_zadan/Task06/Program.cs
+++ Dom_zadan/Task06/Program.cs
@@ -5,3 +5,18 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.Write("Является ли чесло четным? : ");
+int num = int.Parse(Console.ReadLine());
+int remain = num % 2;
+if (remain == 0)
+{
+    Console.WriteLine("Число " + num + " четное?");
+    Console.WriteLine("Четное!");
+}
+else
+{
+    Console.WriteLine("Число " + num + " четное?");
+    Console.WriteLine("Не четное!");
+}
+
