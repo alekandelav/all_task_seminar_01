e6bdd81592a96efda7869ce15524c77fd0a80c3e
alekandelav <140648372+AleksandrLAV@users.noreply.github.com>
2023-08-05 11:12:58 +0300

    число от 1 до N

--- Dom_zadan/Task08/Program.cs
+++ Dom_zadan/Task08/Program.cs
@@ -3,3 +3,16 @@
 // все чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число (N): ");
+
+int num = int.Parse(Console.ReadLine());
+int even = 2;
+
+Console.WriteLine ("ВЫ ВИДИТЕ ЭТИ ЧИСЛА от 1 до N");
+
+while (num >= even)
+{
+    Console.Write(even + " ");
+    even = even + 2;
+}
