3fe3be132d53518a330cecf18cf5a06f5fa703c2
alekandelav <140648372+AleksandrLAV@users.noreply.github.com>
2023-08-22 14:40:35 +0300

    am

---
+++ lessons#3/task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string RangeCoordinates(int quart)
+{
+    if (quart ==1) return "x > 0 ; y > 0";
+    if (quart ==2) return "x < 0 ; y > 0";
+    if (quart ==3) return "x < 0 ; y < 0";
+    if (quart ==4) return "x > 0 ; y < 0";
+    return null;
+}
+Console.WriteLine("Введите координаты точки");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string rangeCoordinates=RangeCoordinates(quarter);
+string result = rangeCoordinates != null 
+? rangeCoordinates
+                    : " Введены некорректные координаты";
+Console.WriteLine(result);
--- lessons#3/task3/Program.cs
+++ lessons#3/task3/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// double sqrt = Math.Sqrt(5);
+// double pow = Math.Pow(3, 5);
+// Console.WriteLine(sqrt);
+// Console.WriteLine(pow);
+
+double d = 5.09983461; // 5.09
+double dRound = Math.Round(d, 5, MidpointRounding.ToZero);
+Console.WriteLine(dRound);
+Console.WriteLine($"{d:F}");
